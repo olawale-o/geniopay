4bc4d35a700b884bd5aec6d1d1f0f3f2e08451fd
olawale-o <omoogunolawale@gmail.com>
2022-03-07 13:46:36 +0100

    Create custom svgs

---
+++ src/components/Svg/index.js
@@ -0,0 +1,6 @@
+export { default as Grid } from './Grid';
+export { default as Wallet } from './Wallet';
+export { default as Card } from './Card';
+export { default as Fx } from './Fx';
+export { default as Beneficiary } from './Beneficiary';
+export { default as Perk } from './Perk';
