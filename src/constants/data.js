40d09660f28382d74f72c3fbefb4d11d2c142f13
olawale-o <omoogunolawale@gmail.com>
2022-03-09 04:22:09 +0100

    Create mock data

---
+++ src/constants/data.js
@@ -0,0 +1,12 @@
+const data = {
+  labels: ['Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+  datasets: [
+    {
+      id: 1,
+      label: '',
+      data: [4, 3.8, 3, 3.8, 3.2, 2.2, 3.5, 3.8],
+    },
+  ],
+};
+
+export default data;
